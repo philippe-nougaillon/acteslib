a1dcc403ab485bbf57334be8f7ec37750264e22c
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-10-13 11:17:01 +0200

    commune : select

--- app/decorators/demande_decorator.rb
+++ app/decorators/demande_decorator.rb
@@ -28,4 +28,8 @@ module DemandeDecorator
     ['Abidjan', 'Bas-Sassandra', 'Comoé', 'Denguélé', 'Gôh-Djiboua', 'Lacs', 'Lagunes', 'Montagnes', 'Sassandra-Marahoué', 'Savanes', 'Vallée du Bandama', 'Woroba', 'Yamoussoukro', 'Zanzan']
   end
 
+  def communes_gôh_djiboua
+    ['Akridou-Laddé', 'Bayota', 'Brihiri', 'Broudoukou-Penda', 'Chiépo', 'Dahiépa-Kéhi', 'Dairo-Didizo', 'Dakouritrohoin', 'Didoko', 'Diégonéfla', 'Dignago', 'Divo', 'Djidji', 'Dougroupalégnoa', 'Doukouya', 'Doukouyo', 'Dousséba', 'Dribouo', 'Gabia', 'Galébouo', 'Gbadjié', 'Gnagbodougnoa', 'Gnakouboué', 'Godélilié 1', 'Godiéko', 'Gogné', 'Goudi', 'Goudouko', 'Gragba-Dagolilié', 'Grobiakoko', 'Guépahouo', 'Guibéroua', 'Hermankono-Diès', 'Hermankono-Garo', 'Hiré', 'Iroporia', 'Kadéko', 'Kosséhoa', 'Kouaméfla', 'Kpapékou', 'Kreuzoukoué', 'Lagaze', 'Lahouda', 'Lakota', 'Lauzoua', 'Ligrohoin', 'Mabouo', 'Maguéhio', 'Néko', 'Niakoblognoa', 'Niambézaria', 'Niazaroko', 'Ogoudou', 'Ouaragahio', 'Oumé', 'Satroko', 'Serihio', 'Tabléguikou', 'Téhiri', 'Tiégba', 'Tonla', 'Toutoubré', 'Yocoboué', 'Yopohoué', 'Yopohué', 'Zégo', 'Zialégréhoa', 'Ziki-Diès', 'Zikisso']
+  end
+
 end
